3c65321640cce4b2e5be0e4cf805833fbd2913de
jackiepro <jackie.hung.phone@gmail.com>
2020-07-13 16:55:14 +0800

    20200713-小米體重

--- mi/mi.py
+++ mi/mi.py
@@ -24,65 +24,55 @@ print(timeStr)
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
-#import datetime
 import time
 #==處理中文字型問題===
 font = {'family' : 'Microsoft JhengHei','weight' : 'bold','size'  : '12'}
 plt.rc('font', **font) 
 #==去掉第一行title===========
 csvRows = []
-in_score_csvfile = open(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY_1594344484383.csv', 'r' ,encoding='utf8')
+#讀取原始檔
+in_score_csvfile = open(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY_1594624781643.csv', 'r' ,encoding='utf8')
 csv_reader = csv.reader(in_score_csvfile)
 for row in csv_reader:
 	if csv_reader.line_num == 1:
 		continue	# 停止第一行的讀取
 	csvRows.append(row)
 in_score_csvfile.close() 
+#開啟暫存檔
 out_tmp_csvfile  = open(r'C:\GitHub\python\PY-Learn\mi\BODY\temp_BODY.csv','w', encoding='utf8',newline='')
 csv_Writer = csv.writer(out_tmp_csvfile)
-for row in csvRows:
+for row in csvRows: #一行一行寫入暫存檔
     if any(field.strip() for field in row):
         csv_Writer.writerow(row)
 out_tmp_csvfile.close()
-#==================
+#==清理所需資料格式================
+#讀取暫存檔
 in_csvfile = open(r'C:\GitHub\python\PY-Learn\mi\BODY\temp_BODY.csv','r' , encoding = 'utf8')
 line = in_csvfile.readline()
+#開啟清理過後的資料檔
 out_csvfile = open(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY.csv','w', encoding = 'utf8')
-out_csvfile_title = ['date','weight']
+out_csvfile_title = ['date','weight'] #只寫入日期與體重
 out_csvfile.write('%s,%s \n' %(out_csvfile_title[0],out_csvfile_title[1]))
-while line != '':
-    data = line.split(',')
-    unix_ts = eval(data[0])
-    print(unix_ts)
-    ltime = time.localtime(unix_ts)
-    print(ltime)
-    ldate = time.strftime("%Y-%m",ltime)
-    print(ldate)
-    lweight = data[1]
-    print(lweight)
-    out_csvfile.write('%s,%s\n' % (ldate,lweight))
-    print('%s,%s\n' % (ldate,lweight))
+while line != '': #用迴圈取出紀錄做整理
+    data = line.split(',') #先分割
+    unix_ts = eval(data[0]) #將unix時間戳記取出
+    ltime = time.localtime(unix_ts) #將unix時間戳記轉換成正常時間
+    ldate = time.strftime("%Y/%m/%d",ltime) #只取出年月日
+    lweight = eval(data[1]) #取出體重
+    out_csvfile.write('%s,%.1f\n' % (ldate,lweight)) #將體重以小數點後一位寫入
     line = in_csvfile.readline()  
 out_csvfile.close()
 in_csvfile.close()
-
-
-#============
-fortune = pd.read_csv(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY.csv')
-#date = fortune.groupby('日期').mean()
-date = fortune.groupby('date').min()
+#==以同日期中取出最小值的體重，再寫入暫存檔==========
+minw = pd.read_csv(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY.csv')
+date = minw.groupby('date').min() #取最小值
 date.to_csv(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY-new.csv',encoding='utf8')
-print(date)
-
 #==分析圖======
-#colName = ['日期','體重']
-weights = pd.read_csv(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY-new.csv', index_col =0)
-print(weights)
-type(weights)
-tw = pd.DataFrame(weights, columns = ['date'], index = weights['weight'])
-plt.plot(tw['date'],tw['weight'])
-plt.title('體重紀錄分析表')
+weights = pd.read_csv(r'C:\GitHub\python\PY-Learn\mi\BODY\BODY-new.csv', index_col=0)
+weights.plot(title = '體重紀錄分析表')
 plt.xlabel('日期')
 plt.ylabel('體重')
 plt.show()
-    
+
+
+
