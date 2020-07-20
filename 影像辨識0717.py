fc2076c2d1c80d9065d1ca7ec80b648319a4da93
jackiepro <jackie.hung.phone@gmail.com>
2020-07-20 17:31:01 +0800

    20200720-上課資料

--- 影像辨識0717.py
+++ 影像辨識0717.py
@@ -369,27 +369,423 @@ cv.waitKey()
 
 #----接續 0717的影像色彩
 
+"""
+Created on Fri Jul 17 16:51:25 2020
 
+@author: Amanda Lu
+"""
+#0717 前為影像辨識 接續色彩
 
-
-
-
-
-
-
-
-
-
+'''色彩空間類型轉換
+        
+    RGB色彩空間:
+        
+        R 紅           G 綠         B 藍
+        三者為重要性相等  "但忽略亮度資訊"
+        為硬體角度的色彩模型 以人眼觀看  存在一定的差異程度
+        
+    HSV(HSB)色彩空間:
+        視覺感知的色彩模型 以心理學及視覺角度 指出人類視覺感知包含三部分:
+            1.色調 2.飽和度 3.明度
+            
+        Hue色調(色相)
+            光的顏色 範圍為 0~360
+            
+            基本款:
+                0   紅色
+                60  黃色
+                120 綠色
+                180 青色
+                240 藍色
+                300 品紅
+        
+        Saturation飽和度
+            色彩顏色深淺程度 為一比例值  範圍 0~1
+            是色彩純度值 與 最大純度值 之比值
+            飽和度為0==灰階
+            
+        Value明暗 
+            光的明暗程度  範圍 0~1
+            
+        
+        
+函數:                     src 原始影像
+    色彩空間轉換  cvtColor(src,mode)
+                                mode 色彩空間轉換
 
  
+    *opneCV色彩模型為 "BGR"  與 RGB 不同
+       RGB / BGR轉換到 GRAY灰階
+        
+       GRAY灰階=0.299*R + 0.587*G + 0.114*B
+'''
+
+#前12在影像辨識
+
+#13
+import cv2 as cv
+import numpy as np 
+img =np.random.randint(0,256,size =[2,4,3],dtype =np.uint8)
+rst =cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+print('img =\n',img)
+print('rst =\n',rst)
+print('像素點(1,0) 直接計算得到的值 =',img[1,0,0]*0.114+img[1,0,1]*0.587+img[1,0,2]*0.299)
+print('像素點(1,0) 使用公式cv.cvColor() 轉換值 = ',rst[1,0])
+            
+#==================================
+#20200720
+
+#ex01:
+
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[2,4],dtype=np.uint8)
+rst = cv.cvtColor(img,cv.COLOR_GRAY2BGR)
+print('img = \n', img)
+print('rst = \n', rst)    
+
+"""
+灰階 <-> BGR
+=>當影像由GRAY彩色空間轉換到BGR色彩空間時,最後所有通道直視相同的
+
+BGR <-> RGB
+=>RGB與BGR互相轉換，R通道值與B通道值互換
+=>BGR轉換為GRAY時，即為灰階影像，在將該影像轉換為BGR時，B、G、R值均相同，顯示結果為灰階
+                                                ^_彩色__^
+                                                ^_ BGR2GRAY
+                                                ^_ GRAY2BGR
+                                                ^_ BGR2RGB
+
+=>色調H值設定範圍為 0~360, 8位元影像每個像素點能表示的灰階等級為2的8次方=256各，
+  故8位元影像要以HSV表示時，必須將H值調整為0~255
+  ->處理方式: 將 H值/2  得到 0~180之間的值  (適應8位元二進位儲存及表示)
+  0:紅 30:黃 60:綠 90:青 120:藍 150:品紅
+
+=>飽和度S值設定範圍為 0~1，因灰階影像之R,G,B值成分相等，相當於一種極度不飽和的色彩，
+  故灰階影像其 飽和度 = 0
+  灰階影像顯示時，較亮區域對應之色彩具有較高飽和度，及低飽和度，所得之色調值"不可靠"!
+
+=>亮度V值設定範圍為0~1， 對應到 0~255
+  BGRHSV
+
+=>HSV色彩主要差異是在H通道，可對H通道進行篩選，已選出特定色彩
+  ->inRange()函數:
+      針對像素點之像素值判斷是否在某範圍內
+      cv2.inRange(src,lower,upper)
+                  ^影像 ^範圍下限 ^範圍上限
+    若src值在範圍內，則傳回值中對應位置上之值為 255 否則為 0  
+
+=>HSV色彩空間分析色彩時並非分析一特定值，而是分析依色彩區間
+  如藍色H值為120，分析藍色時會以其附近值作為區間，通常附近值為10
+  即120+10，120-10作為分析區間，S通道即V通道分析區間在100~255
+  因飽和度、亮度太低時，得到的H值"不可靠" =>顏色判斷會失真
+  
+  HSV色彩區間: [H-10,100,100]~[H+10,255,255]
+  紅色色彩的區間: [0,100,100]~[10,255,255]
+  綠色色彩區間: [50,100,100]~[70,255,255]
+  藍色色彩區間: [110,100,100]~[130,255,255]
+  
+=>膚色分析(HSV): 
+    一般可設定為: 
+        色調值: 5~170  (minHue,maxHue)
+        飽和度: 25~166 (minSat,maxSat)
+
+=>處理並設定HSV值，並將其設定為遮罩(mask:即掩模)，再將影像及遮罩座位元and運算，
+  達到以遮罩控制影像顯示的目的
+  
+=>於RGB/BGR三通道後，在再加一通道，名為alpha，即透明度通道，即為4通道影像，
+  為RGBA/BGRA色彩空間，設定範圍為: 0~1 or 0~255 (透明~不透明)
+  BGR2BGRA
+  
+=>影像縮放:resize()函數:
+    (src,dsize,fx,fy,interpolation)
+    src:原始影像
+    dsize:輸出影像大小
+    fx:水平縮放比例
+    fy:垂直縮放比例
+    interpolation:內插方式
+    影像執行幾何處理時，若無法直接透過對應值獲得像素點新值時，
+    則採用內插處理(通常放大影像使用)
+    內插處理:
+        ->INTER_AREA ->區域內插      -- 鄰近、最接近區域
+        ->INTER_NEAREST ->最鄰近內插  --^
+        ->INTER_CUBIC ->三次樣得內插 --像點附近4*4區域 -->慢
+        ->INTER_LINEAR ->線性內插 --預設 -->快
+
+幾何轉換
+
+#ex13:
+原始影像大小:2行4列
+resize後的大小:4行2列
+結果的行數為原始的列數
+
+shape屬性:
+第一個值:行,第二個值:列
+dsize屬性:
+第一個值:列,第二個值:行   
+
+=>影像翻轉:flip()
+cv2.flip(src,flipcode)
+src:原始影像
+flipcode: 
+    0: 繞X軸翻轉
+    正數:繞Y軸翻轉(1)
+    負數:繞X,Y軸翻轉(-1)      
+ 
+  
+"""
+#ex2:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[2,4,3],dtype=np.uint8)
+rgb = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+bgr = cv.cvtColor(rgb,cv.COLOR_RGB2BGR)
+print('img = \n', img)
+print('rgb = \n', rgb)  
+print('bgr = \n', bgr)    
+
+#ex3:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+bgr = cv.cvtColor(gray,cv.COLOR_GRAY2BGR)
+
+print('img.shape = ', img.shape)
+print('gray.shape = ', gray.shape)  
+print('bgr.shape = ', bgr.shape)
+
+cv.imshow('img',img)
+cv.imshow('gray',gray)  
+cv.imshow('bgr',bgr)
+print('bgr = \n',bgr)
+cv.waitKey()
+
+#ex4:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rgb = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+cv.imshow('img',img)
+cv.imshow('rgb',rgb)
+cv.waitKey()
+
+#ex5:
+import cv2 as cv
+import numpy as np
+imgblue = np.zeros([1,1,3],dtype=np.uint8)
+imgblue[0,0,0] = 255
+Blue = imgblue
+#測試藍色的HSV值
+BlueHSV = cv.cvtColor(Blue,cv.COLOR_BGR2HSV)
+print('Blue = \n',Blue)
+print('BlueHSV = \n',BlueHSV)
+
+imggreen = np.zeros([1,1,3],dtype=np.uint8)
+imggreen[0,0,1] = 255
+Green = imggreen
+GreenHSV = cv.cvtColor(Green,cv.COLOR_BGR2HSV)
+print('Green = \n',Green)
+print('GreenHSV = \n',GreenHSV)
+
+imgred = np.zeros([1,1,3],dtype=np.uint8)
+imgred[0,0,2] = 255
+Red= imgred
+RedHSV = cv.cvtColor(Red,cv.COLOR_BGR2HSV)
+print('Red = \n',Red)
+print('RedHSV = \n',RedHSV)
+
+#ex6:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,255,size=[5,5],dtype=np.uint8)
+min = 100
+max = 200
+mask = cv.inRange(img,min,max)
+print('img = \n',img)
+print('mask = \n',mask)
+
+#ex7:
+import cv2 as cv
+import numpy as np
+img = np.ones([5,5],dtype=np.uint8)*9
+mask = np.zeros([5,5],dtype=np.uint8)
+mask[0:3,0] = 1
+mask[2:5,2:4] = 1
+roi = cv.bitwise_and(img,img,mask=mask)
+print('img = \n',img)
+print('mask = \n',mask)
+print('roi = \n',roi)
 
 
+#ex8:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\opencv-logo.png')
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+cv.imshow('img',img)
+
+minBlue = np.array([110,50,50])
+maxBlue = np.array([133,255,255])
+mask = cv.inRange(hsv,minBlue,maxBlue)
+blue = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('blue',blue)
+
+minGreen = np.array([50,50,50])
+maxGreen = np.array([70,255,255])
+mask = cv.inRange(hsv,minGreen,maxGreen)
+green = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('green',green)
+
+minRed = np.array([0,50,50])
+maxRed = np.array([30,255,255])
+mask = cv.inRange(hsv,minRed,maxRed)
+red = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('red',red)
+cv.waitKey()
+
+#ex9:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+h,s,v = cv.split(hsv)
+minHue = 5
+maxHue = 160
+hueMask = cv.inRange(h,minHue,maxHue)
+minSat = 25
+maxSat = 160
+satMask = cv.inRange(s,minSat,maxSat)
+mask = hueMask & satMask
+roi = cv.bitwise_and(img,img,mask=mask)
+cv.imshow('img',img)
+cv.imshow('ROI',roi)
+cv.waitKey()
+
+#ex10:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[2,3,3],dtype=np.uint8)
+bgra = cv.cvtColor(img,cv.COLOR_BGR2BGRA)
+print('img',img)
+print('bgra',bgra)
+b,g,r,a = cv.split(bgra)
+print('a = \n',a)
+a[:,:] = 125
+bgra = cv.merge([b,g,r,a])
+print('bgra = \n',bgra)
+
+#ex11:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+bgra = cv.cvtColor(img,cv.COLOR_BGR2BGRA)
+b,g,r,a = cv.split(bgra)
+a[:,:] = 125
+bgra125 = cv.merge([b,g,r,a])
+a[:,:] = 0
+bgra0 = cv.merge([b,g,r,a])
+cv.imshow('img',img)
+cv.imshow('bgra',bgra)
+cv.imshow('bgra125',bgra125)
+cv.imshow('bgra0',bgra0)
+cv.waitKey()
+cv.destroyAllWindows()
+cv.imwrite(r'C:\GitHub\python\PY-Learn\bgra.png',bgra)
+cv.imwrite(r'C:\GitHub\python\PY-Learn\bgra125.png',bgra125)
+cv.imwrite(r'C:\GitHub\python\PY-Learn\bgra0.png',bgra0)
+
+#ex12:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena_01.jpg')
+cv.imshow('img',img)
+hsv_low = np.array([0,0,0])
+hsv_high = np.array([0,0,0])
+def h_low(value):
+    hsv_low[0] = value
+def h_high(value):
+    hsv_high[0] = value
+def s_low(value):
+    hsv_low[1] = value
+def s_high(value):
+    hsv_high[1] = value
+def v_low(value):
+    hsv_low[2] = value
+def v_high(value):
+    hsv_high[2] = value
+cv.namedWindow('image')
+cv.createTrackbar('H_low','image',0,250,h_low)
+cv.createTrackbar('H_high','image',0,250,h_high)
+cv.createTrackbar('S_low','image',0,250,s_low)
+cv.createTrackbar('S_high','image',0,250,s_high)
+cv.createTrackbar('V_low','image',0,250,v_low)
+cv.createTrackbar('V_high','image',0,250,v_high)
+while(1):
+    hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    hsv = cv.inRange(hsv,hsv_low,hsv_high)
+    cv.imshow('hsv',hsv)
+    if cv.waitKey(1) & 0xFF == 27:
+        break
+cv.destroyAllWindows()
 
 
+#ex13:
+import cv2 as cv
+import numpy as np
+img = np.ones([2,4,3],dtype=np.uint8)
+size = img.shape[:2]
+rst = cv.resize(img,size)
+print('img.shape = \n',img.shape)
+print('img = \n',img)
+print('rst.shape = \n',rst.shape)
+print('rst = \n',rst) 
 
 
+#ex14:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rows,cols=img.shape[:2]
+size = (int(cols*0.9),int(rows*0.5))
+rst = cv.resize(img,size)
+print('img.shape = \n',img.shape)
+print('rst.shape = \n',rst.shape)
+cv.imshow('img',img)
+cv.imshow('rst',rst)
+cv.waitKey()
+
+#ex15:
+import cv2 as cv
+img1 = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+img2 = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+img3 = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+res1 = cv.resize(img1,None,fx=1.5,fy=1.5,interpolation=cv.INTER_AREA)
+res2 = cv.resize(img2,None,fx=1.5,fy=1.5,interpolation=cv.INTER_LINEAR)
+res3 = cv.resize(img3,None,fx=1.5,fy=1.5,interpolation=cv.INTER_CUBIC)
+cv.imshow('img1',img1)
+cv.imshow('res1',res1)
+cv.imshow('res2',res2)
+cv.imshow('res3',res3)
+cv.imwrite(r'C:\GitHub\python\PY-Learn\lena512_A.jpg',res1)
+cv.imwrite(r'C:\GitHub\python\PY-Learn\lena512_B.jpg',res2)
+cv.imwrite(r'C:\GitHub\python\PY-Learn\lena512_C.jpg',res3)
+while True:
+    if(cv.waitKey() & 0xFF) == ord('q'):
+        break
+    else:
+        pass
+cv.destroyAllWindows()
 
 
+#ex16:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+x = cv.flip(img,0)
+y = cv.flip(img,1)
+xy = cv.flip(img,-1)
+cv.imshow('img',img)
+cv.imshow('x',x)
+cv.imshow('y',y)
+cv.imshow('xy',xy)
+cv.waitKey()
 
 
 
