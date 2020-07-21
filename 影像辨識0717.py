9071a130b34ad18dc49a9eea8680fcc556662970
jackiepro <jackie.hung.phone@gmail.com>
2020-07-21 17:58:48 +0800

    20200721-上課資料

--- 影像辨識0717.py
+++ 影像辨識0717.py
@@ -530,9 +530,106 @@ src:原始影像
 flipcode: 
     0: 繞X軸翻轉
     正數:繞Y軸翻轉(1)
-    負數:繞X,Y軸翻轉(-1)      
- 
-  
+    負數:繞X,Y軸翻轉(-1)    
+    
+#20200721
+    
+=>仿射轉換
+透過幾何轉換執行平移，旋轉...，此轉換可以保持影像的平行性(平行線)及平直性(直線)
+  warpAffine():仿射函數
+  ->cv2.warpAffine(src,M,dsize,flags,borderMode,borderValue)
+    src:影像
+    M:轉換矩陣
+    dsize:輸出影像大小
+    flags:內插方法，預設INTER_LINEAR
+    borderMode:框線模式 BORDER_CONSTANT
+    borderValue:框線值，預設 0
+    
+    1.平移:透過3*3(或2*2)M矩陣執行轉換
+       -->[1 0 tx]
+    M->|           :將每一像點移到x+t,y+t
+       -->[0 1 ty]
+    M(11) = 1       M(21) = 0
+    M(12) = 0       M(22) = 1
+    M(13) = 100     M(23) = 200
+    
+    src(x*1+y*0+100,x*0+y*1+200)
+    
+    getRotationMatrix2D():
+        產生已warpAffine()進行旋轉轉換時的轉換矩陣
+        -> cv2.getRotationMatrix2D(center,angle,scale)
+        center : 旋轉中心點
+        angle　: 旋轉角度(逆時針為正)
+        scale : 縮放比例
+    以原點旋轉:(自訂矩陣)
+    --              --
+   |  cos0  -sin0  0  |
+   |  sin0   cos0  0  | 
+   |  0      0     1  |
+    --              --
+    以(x,y)旋轉:(自訂矩陣)
+     --                         --
+   |  cos0  -sin0  x-xcon0+ysin0  |
+   |  sin0   cos0  y-xsin0-ycos0  | 
+   |  0      0            1       |
+    --                          --
+    
+  getAffineTransform():
+      產生以warpAffine()進行仿射轉換時的轉換矩陣(以三個點進行轉換)
+    ->cv2.getAffineTransform(src,dst)
+    src:影像的三個點座標 -> 二維陣列
+    dst:轉換的三個點座標 ->^
+    
+=>透視轉換:
+    將矩形轉換為任意四邊形(無須維持平行性及平直性)
+    warpPerspective()函數
+    ->warpPerspective(src,M,dsize,flags,corderMode,borderValue)
+      src:影像
+      M:轉換矩陣
+      dsize:輸出影像大小
+      flags:內插方法，預設INTER_LINEAR
+      borderMode:框線模式 BORDER_CONSTANT
+      borderValue:框線值，預設 0
+      
+    getPerspectiveTransform():
+      產生以 warpPerspective()執行時得轉換矩陣M
+    ->cv2.getPerspectiveTransform(src,dst)
+    src:原影像的四個點座標  --->任意四邊形，無須平行性，平直性
+    dst:轉換影像的四個點座標 -->^
+
+=>重對映:
+    將影像中的像素點放到另一位置
+    remap()函數
+    ->cv2.remap(src,x,y,interpolation,corderMode,borderValue)
+    src : 原始影像
+    x : 對映的X座標
+    y : 對映的Y座標
+    
+   (rst)
+   目標陣列中的所有像素點均對映到原影像(img)中之第0行,第3列之像素點
+   
+   繞x軸翻轉:
+       ->對映過程中->
+                   x軸座標值不變
+                   y軸座標值以x軸作為對稱軸進行交換
+       
+       ->對稱關係為:(行號索引自0開始)
+                  目前行號+對稱行號 = 總行數-1      
+       ->反映於X,Y上:(mapx,mapy)
+                  mapx值保持不變,mapy=總行數-1-目前行號
+    
+    X,Y軸互換:
+        mapx調整為所在的行號
+        mapy調整為所在的列號
+        若行列數量不相同，則無法完成對映的值會設為0
+        .像點重對映
+        .陣列複製(影像複製)
+        .繞x軸翻轉
+        .繞y軸翻轉
+        .繞x,y軸翻轉
+        .x,y軸互換
+        .縮放
+    
 """
 #ex2:
 import cv2 as cv
@@ -787,6 +884,378 @@ cv.imshow('y',y)
 cv.imshow('xy',xy)
 cv.waitKey()
 
+#20200721
+
+#ex1:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+height,width = img.shape[:2]
+x = 100
+y = 200
+M = np.float32([[1,0,x],[0,1,y]])
+move = cv.warpAffine(img,M,(width,height))
+cv.imshow('img',img)
+cv.imshow('move',move)
+cv.waitKey()
+
+#ex2:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+height,width = img.shape[:2]
+M = cv.getRotationMatrix2D((width/2,height/2),45,0.6)
+rotate = cv.warpAffine(img,M,(width,height))
+cv.imshow('img',img)
+cv.imshow('rotate',rotate)
+cv.waitKey()
+
+#ex3:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rows,cols,ch = img.shape
+p1 = np.float32([[0,0],[cols-1,0],[0,rows-1]])
+p2 = np.float32([[0,rows*0.33],[cols*0.85,rows*0.25],[cols*0.15,rows*0.7]])
+M = cv.getAffineTransform(p1,p2)
+dst = cv.warpAffine(img,M,(cols,rows))
+cv.imshow('img',img)
+cv.imshow('result',dst)
+cv.waitKey()
+
+#ex4:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rows,cols = img.shape[:2]
+print(rows,cols)
+pts1 = np.float32([[150,50],[300,50],[60,450],[210,450]])
+pts2 = np.float32([[50,50],[rows-50,50],[50,cols-50],[rows-50,cols-50]])
+M = cv.getPerspectiveTransform(pts1,pts2)
+dst = cv.warpPerspective(img,M,(cols,rows))
+cv.imshow('img',img)
+cv.imshow('dst',dst)
+cv.waitKey()
+
+#ex5:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\opencv-logo.png')
+rows,cols = img.shape[:2]
+print(rows,cols)
+pts1 = np.float32([[300,290],[500,290],[300,400],[500,400]])
+pts2 = np.float32([[50,50],[rows-170,100],[50,cols+100],[rows-240,cols-200]])
+M = cv.getPerspectiveTransform(pts1,pts2)
+dst = cv.warpPerspective(img,M,(cols,rows))
+cv.imshow('img',img)
+cv.imshow('dst',dst)
+cv.waitKey()
+
+#ex6:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[4,5],dtype=np.uint8)
+rows,cols = img.shape
+mapx = np.ones(img.shape,np.float32)*3
+mapy = np.ones(img.shape,np.float32)*0
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+print('img = \n',img)
+print('mapx = \n',mapx)
+print('mapy = \n',mapy)
+print('rst = \n',rst)
+
+
+#ex7:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[4,5],dtype=np.uint8)
+rows,cols = img.shape
+mapx = np.ones(img.shape,np.float32)
+mapy = np.ones(img.shape,np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),j)
+        mapy.itemset((i,j),i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+print('img = \n',img)
+print('mapx = \n',mapx)
+print('mapy = \n',mapy)
+print('rst = \n',rst)
+
+
+#ex7-1:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rows,cols = img.shape[:2]
+mapx = np.ones(img.shape[:2],np.float32)
+mapy = np.ones(img.shape[:2],np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),j)
+        mapy.itemset((i,j),i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+cv.imshow('img',img)
+cv.imshow('rst',rst)
+cv.waitKey()
+
+
+#ex8:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[4,5],dtype=np.uint8)
+rows,cols = img.shape
+mapx = np.ones(img.shape,np.float32)
+mapy = np.ones(img.shape,np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),j)
+        mapy.itemset((i,j),rows-1-i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+print('img = \n',img)
+print('mapx = \n',mapx)
+print('mapy = \n',mapy)
+print('rst = \n',rst)
+
+
+#ex9:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[4,5],dtype=np.uint8)
+rows,cols = img.shape
+mapx = np.zeros(img.shape,np.float32)
+mapy = np.zeros(img.shape,np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),cols-1-j)
+        mapy.itemset((i,j),i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+print('img = \n',img)
+print('mapx = \n',mapx)
+print('mapy = \n',mapy)
+print('rst = \n',rst)
+
+
+#ex9-1:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rows,cols = img.shape[:2]
+mapx = np.zeros(img.shape[:2],np.float32)
+mapy = np.zeros(img.shape[:2],np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),cols-1-j)
+        mapy.itemset((i,j),i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+cv.imshow('img',img)
+cv.imshow('rst',rst)
+cv.waitKey()
+
+
+#ex10:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[4,5],dtype=np.uint8)
+rows,cols = img.shape
+mapx = np.zeros(img.shape,np.float32)
+mapy = np.zeros(img.shape,np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),cols-1-j)
+        mapy.itemset((i,j),rows-1-i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+print('img = \n',img)
+print('mapx = \n',mapx)
+print('mapy = \n',mapy)
+print('rst = \n',rst)
+
+#ex10-1:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg')
+rows,cols = img.shape[:2]
+mapx = np.zeros(img.shape[:2],np.float32)
+mapy = np.zeros(img.shape[:2],np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),cols-1-j)
+        mapy.itemset((i,j),rows-1-i)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+cv.imshow('img',img)
+cv.imshow('rst',rst)
+cv.waitKey()
+
+
+#ex11:
+import cv2 as cv
+import numpy as np
+img = np.random.randint(0,256,size=[4,6],dtype=np.uint8)
+rows,cols = img.shape
+mapx = np.zeros(img.shape,np.float32)
+mapy = np.zeros(img.shape,np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),i)
+        mapy.itemset((i,j),j)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+print('img = \n',img)
+print('mapx = \n',mapx)
+print('mapy = \n',mapy)
+print('rst = \n',rst)
+
+
+#ex11-1:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\opencv-logo.png')
+rows,cols = img.shape[:2]
+mapx = np.zeros(img.shape[:2],np.float32)
+mapy = np.zeros(img.shape[:2],np.float32)
+for i in range(rows):
+    for j in range(cols):
+        mapx.itemset((i,j),i)
+        mapy.itemset((i,j),j)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+cv.imshow('img',img)
+cv.imshow('rst',rst)
+cv.waitKey()
+
+
+#ex12:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena_01.jpg')
+rows,cols = img.shape[:2]
+mapx = np.zeros(img.shape[:2],np.float32)
+mapy = np.zeros(img.shape[:2],np.float32)
+for i in range(rows):
+    for j in range(cols):
+        if 0.25*cols<i<0.75*cols and 0.25*rows<j<0.75*rows:
+            mapx.itemset((i,j),2*(j-cols*0.25)+0.5)
+            mapy.itemset((i,j),2*(i-rows*0.25)+0.5)
+        else:
+            mapx.itemset((i,j),0)
+            mapy.itemset((i,j),0)
+rst = cv.remap(img,mapx,mapy,cv.INTER_LINEAR)
+cv.imshow('img',img)
+cv.imshow('rst',rst)
+cv.waitKey()
+
+
+"""
+影像平滑處理
+
+=>平滑處理及影像濾波
+    ->在儲存影像細節特徵之條件下，對影像進行雜訊(噪點)抑制
+
+=>濾波的目的:
+    ->清除(抑制)雜訊
+
+=>均值濾波:
+    以目前像素點周圍 n*n 範圍像素值之平均值
+    均值:既平均值，將影像先找一個中心點，以n*n範圍的像素值，加總平均，寫入覆蓋原像素值.
+        通常用於銳利度較高的影像，使其顯示較為自然
+    blur()函數:均值濾波
+    ->cv2.blur(src,ksize,anchor,borderType)
+    src : 影像
+    ksize : 濾波核大小，均值處理中鄰近範圍的高度、寬度
+    anchor : 錨點，目前計算均值的點，位於該核中心點的位置，使用預設值即可(-1,-1)
+    borderType : 變化樣式
+    
+    @@@ np.hstack : 水平方向堆疊
+        np.vstack : 垂直方向堆疊
+    
+=>方框濾波:
+    可自行設定濾波結果為平均值或加總覆蓋原像素值
+    boxFilter()函數
+    ->cv2.boxFilter(src,ddepth,ksize,anchor,normalize,borderType)
+    src : 影像
+    ddepth : 影像深度，每一像素能展示的顏色數，一般使用-1表示與原始像素採用相同的影像深度
+    ksize : 濾波核大小，均值處理中鄰近範圍的高度、寬度
+    anchor : 錨點，目前計算均值的點，位於該核中心點的位置，使用預設值即可(-1,-1)
+    normlize : 1 : 即為均值濾波，功能同blur()，預設值.
+               0 : 加總，若加總超過255(最大值)，則會得到白色影像
+    borderType : 變化樣式
 
 
 
+
+"""
+
+
+#ex13:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512_noisy.png',0)
+r5 = cv.blur(img,(5,5))
+r30 = cv.blur(img,(30,30))
+cv.imshow('img',img)
+cv.imshow('r5',r5)
+cv.imshow('r30',r30)
+cv.waitKey()
+
+
+#ex14: 影像加噪點
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512.jpg',1)
+rows, cols, ch = img.shape
+for i in range(10000):
+    x = np.random.randint(0,rows)
+    y = np.random.randint(0,cols)
+    img[x,y,:] = 255
+cv.imshow('noise',img)
+cv.imwrite(r'C:\GitHub\python\PY-Learn\lena512_noisy1.jpg',img)
+cv.waitKey()
+
+
+
+#ex14-1:
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512_noisy1.jpg',1)
+r5 = cv.blur(img,(5,5))
+r30 = cv.blur(img,(30,30))
+cv.imshow('img',img)
+cv.imshow('r5',r5)
+cv.imshow('r30',r30)
+cv.waitKey()
+
+#ex15:
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512_noisy1.jpg',1)
+rst = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+final = cv.blur(rst,(5,5))
+titles = ['Source','Blur']
+images = [rst,final]
+for i in range(2):
+    plt.subplot(1,2,i+1),plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+#ex16:
+import cv2 as cv
+import numpy as np
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512_noisy.png',1)
+bl = np.hstack([cv.blur(img,(3,3)),cv.blur(img,(8,8)),cv.blur(img,(15,15))])
+cv.imshow('img',img)
+cv.imshow('result',bl)
+cv.waitKey()
+
+
+#ex17: #方框濾波 預設值
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512_noisy.png',0)
+r = cv.boxFilter(img,-1,(5,5))
+cv.imshow('img',img)
+cv.imshow('result',r)
+cv.waitKey()
+
+#ex18: #方框濾波 加總
+import cv2 as cv
+img = cv.imread(r'C:\GitHub\python\PY-Learn\lena512_noisy.png',0)
+r = cv.boxFilter(img,-1,(5,5),normalize=0)
+cv.imshow('img',img)
+cv.imshow('result',r)
+cv.waitKey()
