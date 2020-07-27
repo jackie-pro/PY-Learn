9aef55ba09525af88e01f6ce8100ab5188963c5f
jackiepro <jackie.hung.phone@gmail.com>
2020-07-27 17:58:52 +0800

    20200727-上課資料

--- 影像辨識0717.py
+++ 影像辨識0717.py
@@ -1696,9 +1696,258 @@ for(x,y,w,h) in faces:
 cv.imshow('img',img)
 cv.waitKey()
 
+#20200727
+"""
+影像辨識(機器)->速度、準確
+(臉、物品、文字)  
+=>影像分類器->載入影像進入辨識
+   |  |         ^差異(角度、方向、訊訊、面積、色彩.....)
+   |  |->經影像處理、運算....->不同的演算及處理方法-> 各自特色(分類器)
+   |  |                      ^辨識結果:盡量提高90%以上
+   |  |->產生分類器(調教個參數)------------------>^
+   -> 自訓練分類->機器學習(深度學習)->各種機器/深度學習演算法
+   
+=>辨識流程:
+    ->設定分類器檔案(*.xml)路徑位置
+    ->建立分類器物件(載入上述位置)
+    ->讀入待辨識影像
+    ->由分類器物件執行偵測影像(人臉)->傳回偵測到的範圍
+    ->繪製圖形標註偵測到的影像範圍
+"""
+
+#ex1:
+import cv2 as cv
+pict = r'C:\GitHub\python\PY-Learn\haarcascade_frontalface_default.xml'
+faceCascade = cv.CascadeClassifier(pict)
+img = cv.imread(r'C:\GitHub\python\PY-Learn\person_3.jpg')
+faces = faceCascade.detectMultiScale(img,scaleFactor=1.1,minNeighbors=9,minSize=(30,30),flags = cv.CASCADE_SCALE_IMAGE)
+
+cv.rectangle(img,(10,img.shape[0]-20),(110,img.shape[0]),(0,0,0),-1)
+cv.putText(img,"find "+str(len(faces))+" face!",(10,img.shape[0]-5),cv.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+
+for(x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(128,255,0),2)
+cv.namedWindow("facedetect")
+cv.imshow('facedetect',img)
+cv.waitKey()
+
+#ex2:
+import cv2 as cv
+pict = r'C:\GitHub\python\PY-Learn\haarcascade_frontalface_default.xml'
+face_Cascade = cv.CascadeClassifier(pict)
+img = cv.imread(r'C:\GitHub\python\PY-Learn\face_execuite.jpg')
+faces = faceCascade.detectMultiScale(img,scaleFactor=1.1,minNeighbors=2,minSize=(20,20))
+
+cv.rectangle(img,(img.shape[1]-140,img.shape[0]-20),(img.shape[1],img.shape[0]),(0,255,255),-1)
+        
+cv.putText(img,"find "+str(len(faces))+" face!",(img.shape[1]-135,img.shape[0]-5),cv.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),1)
+
+for(x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+cv.namedWindow('Face',cv.WINDOW_NORMAL)  #視窗可調整大小
+cv.imshow('Face',img)
+cv.waitKey()
+
+
+#ex3:
+import cv2 as cv
+pict = r'C:\GitHub\python\PY-Learn\haarcascade_frontalface_default.xml' #載入分類器
+face_Cascade = cv.CascadeClassifier(pict)  #建立分類器物件(載入上述位置)
+eyepict = r'C:\GitHub\python\PY-Learn\haarcascade_eye.xml' #載入分類器
+eye_Cascade = cv.CascadeClassifier(eyepict)  #建立分類器物件(載入上述位置)
+
+img = cv.imread(r'C:\GitHub\python\PY-Learn\Lena_01.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(img,scaleFactor=1.15,minNeighbors=10,minSize=(20,20))
+
+
+for(x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    
+    eyes = eye_Cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+cv.imshow('img',img)
+cv.waitKey()
+
+
+#ex4:
+import cv2 as cv
+from PIL import Image #
+case_path = r'C:\GitHub\python\PY-Learn\haarcascade_frontalface_default.xml' #載入分類器
+faceCascade = cv.CascadeClassifier(case_path)  #建立分類器物件(載入上述位置)
+
+imagename = r'C:\GitHub\python\PY-Learn\person_8.jpg'
+image = cv.imread(imagename)
+faces = faceCascade.detectMultiScale(image,scaleFactor=1.1,minNeighbors=5,minSize=(5,5),flags = cv.CASCADE_SCALE_IMAGE)
+
+count =1
+for(x,y,w,h) in faces:
+    cv.rectangle(image,(x,y),(x+w,y+h),(128,255,0),2)
+    filename = str(count)+ '.jpg'
+    image1 = Image.open(imagename)
+    image2 = image1.crop((x,y,x+w,y+h)) 
+                    #^裁切
+    image3 = image2.resize((200,200),Image.ANTIALIAS)
+                    #^重製大小               ^影像最佳化
+    res = r'C:\GitHub\python\PY-Learn\face' + filename
+    image3.save(res)
+    count += 1    
+cv.namedWindow('facedetect')
+cv.imshow('facedetect',image)
+cv.waitKey()
+
+
+#ex5:
+import cv2 as cv
+from PIL import Image #
+pictpath = r'C:\GitHub\python\PY-Learn\haarcascade_frontalface_default.xml' #載入分類器
+face = cv.CascadeClassifier(pictpath)  #建立分類器物件(載入上述位置)
+
+img = cv.imread(r'C:\GitHub\python\PY-Learn\people_01.jpg')
+
+faces = faceCascade.detectMultiScale(img,scaleFactor=1.1,minNeighbors=5,minSize=(10,10))
+
+cv.rectangle(img,(img.shape[1]-140,img.shape[0]-20),(img.shape[1],img.shape[0]),(0,255,255),-1)
+        
+cv.putText(img,"Finded "+str(len(faces))+" face!",(img.shape[1]-135,img.shape[0]-5),cv.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),1)
+
+num =1
+for(x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    filename = str(num)+ '.jpg'
+    image = Image.open(r'C:\GitHub\python\PY-Learn\people_01.jpg')
+    imageCrop = image.crop((x,y,x+w,y+h)) 
+                    #^裁切
+    imageResize = imageCrop.resize((150,150),Image.ANTIALIAS)
+                    #^重製大小               ^影像最佳化
+    res = r'C:\GitHub\python\PY-Learn\face2_' + filename
+    imageResize.save(res)
+    num += 1    
+cv.imshow('Face',img)
+cv.waitKey()
+
+
+#ex6:
+import cv2 as cv
+face_cascade = cv.CascadeClassifier(r'C:\GitHub\python\PY-Learn\haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier(r'C:\GitHub\python\PY-Learn\haarcascade_eye.xml')
+mouth_cascade = cv.CascadeClassifier(r'C:\GitHub\python\PY-Learn\haarcascade_mcs_mouth.xml')
+nose_cascade = cv.CascadeClassifier(r'C:\GitHub\python\PY-Learn\haarcascade_mcs_nose.xml')
+leftear_cascade = cv.CascadeClassifier(r'C:\GitHub\python\PY-Learn\haarcascade_mcs_leftear.xml')
+rightear_cascade = cv.CascadeClassifier(r'C:\GitHub\python\PY-Learn\haarcascade_mcs_rightear.xml')
+img = cv.imread(r'C:\GitHub\python\PY-Learn\Lena_01.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+#face detect
+faces = face_cascade.detectMultiScale(gray,scaleFactor=1.2,minNeighbors=3)
+for (x,y,w,h) in faces:
+    img = cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    
+    eyes = eye_cascade.detectMultiScale(roi_gray,scaleFactor=1.3,minNeighbors=5,minSize=(30,30))
+    for (ex,ey,ew,eh) in eyes:
+        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+        
+    mouth = mouth_cascade.detectMultiScale(roi_gray,scaleFactor=1.4,minNeighbors=8,minSize=(10,10))    
+    for (mx,my,mw,mh) in mouth:
+        cv.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(0,255,0),2)
+        
+    nose = nose_cascade.detectMultiScale(roi_gray,scaleFactor=1.2,minNeighbors=5,minSize=(30,30))    
+    for (nx,ny,nw,nh) in nose:
+        cv.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,0,255),2)
+        
+    leftear = leftear_cascade.detectMultiScale(roi_gray,scaleFactor=1.01,minNeighbors=2,minSize=(30,30))    
+    for (lx,ly,lw,lh) in leftear:
+        cv.rectangle(roi_color,(lx,ly),(lx+lw,ly+lh),(0,0,0),2)
+
+    rightear = rightear_cascade.detectMultiScale(roi_gray,scaleFactor=1.01,minNeighbors=2,minSize=(30,30))    
+    for (rx,ry,rw,rh) in rightear:
+        cv.rectangle(roi_color,(rx,ry),(rx+rw,ry+rh),(0,0,0),2)
+
+cv.imshow('img',img)
+cv.waitKey()
+
+
+"""
+Dlib:(C++函式庫)
+=>用於機器學習，影像處理，影像辨識
+=>人臉監測演算法:
+    ->方向梯度直方圖(HOG)
+    ->線性分類器(Linear Classifier)
+    ->影像金字塔(image pyramid)
+    ->滑動窗格(sliding window)
+=>演算結果得到一分數值
+  分數越大，越接近人臉
+  分數越小，越接近誤判
+  ----------------------
+  建立python版本的環境:
+  1.以Anaconda Navigator建立:
+    Environments -> Create
+  2.以命令列建立:
+    a.  在 AnacondaPrompt系統管理員模式建立3.6環境 => conda create --name python_36 python=3.6
+    b.  到Anaconda Navigator ，搜尋 uninstall spyder ，然後點選安裝spyder 3.6 
+    c.  到Anaconda Navigator 開啟3.6命令列(Termanil)， 安裝dlib => pip install C:\GitHub\python\PY-Learn\dlib-19.8.1-cp36-cp36m-win_amd64.whl
+    d.  繼續在3.6命令列(Termanil)，安裝openCV => pip install opencv-python
+
+=> get_frontal_face_detector 函式
+格式: 變數 = dlib.get_frontal_face_detector() 
+     產生正面臉部偵測元件
+     
+     偵測人臉:
+         結果 = 變數(待偵測影像,偵測參數,分數參數)
+                             ^ 0 => 一般偵測
+                               1 => 人臉範圍較小
+    @安裝 imutils => pip install imutils (提供影像編輯功能)
+
+
+"""
+
+#ex7:
+import dlib
+import cv2 
+import imutils
+img = cv2.imread(r'C:\GitHub\python\PY-Learn\person_8.jpg')
+img = imutils.resize(img,width=1280)
+detector = dlib.get_frontal_face_detector()
+face_rects = detector(img,1)
+
+for i,d in enumerate(face_rects):
+    x1 = d.left()
+    y1 = d.top()
+    x2 = d.right()
+    y2 = d.bottom()
+    cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),4,cv2.LINE_AA)
+cv2.imshow('Face_Detection',img)
+cv2.waitKey()
 
 
 
+#ex8:
+import dlib
+import cv2 
+import imutils
+img = cv2.imread(r'C:\GitHub\python\PY-Learn\face_trump01.jpg')
+#img = cv2.imread(r'C:\GitHub\python\PY-Learn\face_execuite.jpg')
+img = imutils.resize(img,width=1280)
+detector = dlib.get_frontal_face_detector()
+#偵測人臉,輸出分數
+face_rects,scores,idx = detector.run(img,2,-0.1)
+
+for i,d in enumerate(face_rects):
+    x1 = d.left()
+    y1 = d.top()
+    x2 = d.right()
+    y2 = d.bottom()
+    text = "%2.2f(%d)" % (scores[i],idx[i])
+    cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),4,cv2.LINE_AA)
+    #標示分數
+    cv2.putText(img,text,(x1,y1),cv2.FONT_HERSHEY_DUPLEX,0.7,(255,255,255),1,cv2.LINE_AA)
+cv2.imshow('Face_Detection',img)
+cv2.waitKey()
 
 
 
